ba52f6d96b0152d93ef5cab95d48b029a691faa3
wangrl2016 <wangrl2016@gmail.com>
2021-02-22 13:54:15 +0800

    有序链表的优先队列

--- algs4/PriorityElementary.java
+++ algs4/PriorityElementary.java
@@ -98,6 +98,88 @@ public class PriorityElementary {
         }
     }
 
+    private class PriorityQueueOrderedLinkedList<Key extends Comparable<Key>> {
+        // 双向链表
+        private class Node {
+            Key key;
+            Node previous;
+            Node next;
+        }
+
+        // 链表的大小
+        private int size = 0;
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        // 最小值
+        private Node first;
+        // 最大值
+        private Node last;
+
+        // O(N)
+        // 有顺序地插入
+        public void insert(Key key) {
+            // 0 or 1 elements in the list.
+            if (size == 0 || less(key, first.key)) {
+                Node oldFirst = first;
+
+                first = new Node();
+                first.key = key;
+                first.next = oldFirst;
+
+                if (oldFirst != null) {
+                    oldFirst.previous = first;
+                }
+
+                if (first.next == null) {
+                    last = first;
+                }
+            } else {
+                Node current = first;
+
+                // 寻找插入的位置
+                while (current.next != null && less(current.next.key, key))
+                    current = current.next;
+
+                Node newNode = new Node();
+                newNode.key = key;
+                newNode.next = current.next;
+                current.next = newNode;
+                newNode.previous = current;
+
+                if (newNode.next == null) {
+                    last = newNode;
+                } else {
+                    newNode.next.previous = newNode;
+                }
+            }
+            size++;
+        }
+
+        // O(1)
+        public Key removeMax() {
+            if (isEmpty())
+                throw new RuntimeException("Priority queue underflow");
+
+            Key maxValue = last.key;
+
+            last = last.previous;
+
+            if (last != null) {
+                last.next = null;
+            }
+
+            size--;
+            return maxValue;
+        }
+    }
+
     // Exchange a[i] and a[j].
     private static void exchange(Object[] a, int i, int j) {
         Object swap = a[i];
@@ -111,7 +193,7 @@ public class PriorityElementary {
     }
 
     public static void main(String[] args) {
-        Integer[] a = {2, 10, 4, 1};
+        Integer[] a = {2, 10, 4, 1, 5};
 
         // ordered array
         PriorityQueueOrderedArray<Integer> orderedArray =
@@ -136,5 +218,21 @@ public class PriorityElementary {
             StdOut.print(unorderedArray.removeMax() + " ");
 
         StdOut.println();
+
+        // ordered linked list
+        PriorityQueueOrderedLinkedList<Integer> orderedLinkedList =
+                new PriorityElementary().new PriorityQueueOrderedLinkedList<>();
+
+        for (Integer integer: a)
+            orderedLinkedList.insert(integer);
+
+        while (!orderedLinkedList.isEmpty())
+            StdOut.print(orderedLinkedList.removeMax() + " ");
+
+        StdOut.println();
+
+        // unordered linked list
+
+
     }
 }
