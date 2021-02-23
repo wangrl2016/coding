932468f0aca82002e4731ecc462e9ae63b44808c
wangrl2016 <wangrl2016@gmail.com>
2021-02-23 09:22:53 +0800

    实现简单的数组旋转

--- sdsa/ArrayRotate.java
+++ sdsa/ArrayRotate.java
@@ -1,13 +1,32 @@
 public class ArrayRotate {
+    // 方法一
+    public void rotateUsingTempArray(int[] arr, int d) {
+        assert d < arr.length;
+
+        // 1. 将前面d个元素存入到临时数组中
+        int[] temp = new int[d];
+        for (int i = 0; i < d; i++)
+            temp[i] = arr[i];
+
+        // 2. 将剩下的元素进行平移
+        for (int i = 0; i < arr.length - d; i++)
+            arr[i] = arr[i + d];
+
+        // 3. 将临时数组复制到数组中
+        for (int i = 0; i < d; i++)
+            arr[arr.length - d + i] = temp[i];
+    }
+
     // Left rotate arr[] of size n by d.
-    void leftRotateByOne(int arr[]) {
+    private void leftRotateByOne(int arr[]) {
         int temp = arr[0];
         for (int i = 0; i < arr.length - 1; i++)
             arr[i] = arr[i + 1];
         arr[arr.length - 1] = temp;
     }
 
-    void leftRotate(int arr[], int d) {
+    // 方法二
+    public void leftRotate(int arr[], int d) {
         for (int i = 0; i < d; i++)
             leftRotateByOne(arr);
     }
@@ -15,6 +34,13 @@ public class ArrayRotate {
     public static void main(String[] args) {
         ArrayRotate rotate = new ArrayRotate();
         int arr[] = {1, 2, 3, 4, 5, 6, 7};
+
+        rotate.rotateUsingTempArray(arr, 2);
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+
         rotate.leftRotate(arr, 2);
         for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
