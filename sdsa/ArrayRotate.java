c7b15f89e3a34216f72c3574ddcc0a660a380a18
wangrl2016 <wangrl2016@gmail.com>
2021-02-23 13:46:36 +0800

    使用Juggling算法实现数组旋转

--- sdsa/ArrayRotate.java
+++ sdsa/ArrayRotate.java
@@ -31,18 +31,60 @@ public class ArrayRotate {
             leftRotateByOne(arr);
     }
 
+    int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+
+    // 方法三
+    public void jugglingRotate(int arr[], int d) {
+        // To handle if d >= n
+        d = d % arr.length;
+
+        int gcd = gcd(arr.length, d);
+
+        int temp;   // 存放临时数据
+        int j;
+        for (int i = 0; i < gcd; i++) {
+            // Move i-th values of blocks.
+            temp = arr[i];
+            j = i;
+
+            while (true) {
+                int k = j + d;  // 移动数据的位置
+                if (k >= arr.length)
+                    k = k - arr.length; // 超过数组边界
+                if (k == i)
+                    break;  // 循环到开始部分
+                arr[j] = arr[k];
+                j = k;
+            }
+            arr[j] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         ArrayRotate rotate = new ArrayRotate();
-        int arr[] = {1, 2, 3, 4, 5, 6, 7};
 
-        rotate.rotateUsingTempArray(arr, 2);
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
+        int a1[] = {1, 2, 3, 4, 5, 6, 7};
+        rotate.rotateUsingTempArray(a1, 2);
+        for (int i = 0; i < a1.length; i++)
+            System.out.print(a1[i] + " ");
 
         System.out.println();
 
-        rotate.leftRotate(arr, 2);
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
+        int a2[] = {1, 2, 3, 4, 5, 6, 7};
+        rotate.leftRotate(a2, 2);
+        for (int i = 0; i < a2.length; i++)
+            System.out.print(a2[i] + " ");
+
+        System.out.println();
+
+        int a3[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        rotate.jugglingRotate(a3, 3);
+        for (int i : a3)
+            System.out.print(i + " ");
     }
 }
