3632f840cfc5a0029fe65633c07cbe231a193bb4
wangrl2016 <wangrl2016@gmail.com>
2021-03-05 17:36:02 +0800

    完成pbm格式

--- codec/README.md
+++ codec/README.md
@@ -1,2 +1,5 @@
 编解码
 
+[PBM格式](https://zh.wikipedia.org/wiki/PBM格式)
+
+PBM是单色，PGM是灰度图，PPM使用RGB颜色。
--- sdsa/stack/LinkedListArray.java
+++ sdsa/stack/LinkedListArray.java
@@ -20,7 +20,7 @@ public class LinkedListArray {
     }
 
     public void push(int data) {
-        
+
     }
 
 }
