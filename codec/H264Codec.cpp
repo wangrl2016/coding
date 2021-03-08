8125aa5d423431112b27e34485d0aaf033d8b14f
wangrl2016 <wangrl2016@gmail.com>
2021-03-08 23:02:45 +0800

    增加新的编解码

---
+++ codec/H264Codec.cpp
@@ -0,0 +1,8 @@
+//
+// Created by wangrl on 2021/3/8.
+//
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
--- sdsa/binarytree/BinarySearchTree.java
+++ sdsa/binarytree/BinarySearchTree.java
@@ -7,7 +7,7 @@ package binarytree;
  */
 
 public class BinarySearchTree {
-    class Node {
+    static class Node {
         int key;
         Node left, right;
 
@@ -19,4 +19,23 @@ public class BinarySearchTree {
 
     // 根节点
     Node root;
+
+    static Node insert(Node root, int key) {
+        // Tree is empty.
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        // Traverse the tree.
+        if (key < root.key)
+            // Insert in the left subtree.
+            root.left = insert(root.left, key);
+        else if (key > root.key)
+            // Insert in the right subtree.
+            root.right = insert(root.right, key);
+
+        return root;
+    }
+
 }
