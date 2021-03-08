73fb7207e1912fe25b59554b7056b7c23884e3cb
wangrl2016 <wangrl2016@gmail.com>
2021-03-08 17:29:18 +0800

    项目需要的依赖

---
+++ git_sync_deps.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+# 项目所需要的依赖
+dependencies = {
+
+}
+
+
+def status(directory, commit_hash, change):
+    def truncate(s, length):
+        return s if len(s) <= length else s[:(length - 3)] + '...'
+
+    dir_len = 36
+    directory = truncate(directory, dir_len)
+    commit_hash = truncate(commit_hash, 40)
+    symbol = '>' if change else '@'
+    sys.stdout.write('%-*s %s %s\n' % (dir_len, directory, symbol, commit_hash))
+
+
+def is_sha1_sum(s):
+    """
+    SHA1 sums are 160 bits, encoded as lowercase hexadecimal.
+    """
+    return len(s) == 40 and all(c in '0123456789abcdef' for c in s)
+
+
+def git_executable():
+    """
+    Find the git executable.
+    :return: A string suitable for passing to subprocess functions, or None.
+    """
+    with open(os.devnull, 'w') as devnull:
+        try:
+            subprocess.run(['git', '--version'], stdout=devnull)
+        except (OSError,):
+            return None
+        return 'git'
+
+
+def git_sync_deps():
+    git = git_executable()
+    assert git
+
+    for directory in sorted(dependencies):
+        if '@' in dependencies[directory]:
+            repo, commit_hash = dependencies[directory].split('@', 1)
+        else:
+            raise Exception("Please specify commit")
+
+        if not is_sha1_sum(commit_hash):
+            raise Exception("Poorly formed commit hash: %r" % commit_hash)
+
+        if not os.path.isdir(directory):
+            subprocess.run([git, 'clone', '--quiet', '--no-checkout', repo, directory])
+            subprocess.run([git, 'checkout', '--quiet', commit_hash], cwd=directory)
+
+            status(directory, commit_hash, False)  # success
+            continue
+
+        with open(os.devnull, 'w') as devnull:
+            # If this fails, we will fetch before trying again.
+            # Don't spam user with error information.
+            if 0 == subprocess.run([git, 'checkout', '--quiet', commit_hash],
+                                   cwd=directory, stderr=devnull).returncode:
+                status(directory, commit_hash, False)  # success
+                continue
+
+        subprocess.run([git, 'remote', 'set-url', 'origin', repo], cwd=directory)
+        subprocess.run([git, 'fetch', '--quiet'], cwd=directory)
+        subprocess.run([git, 'checkout', '--quiet', commit_hash], cwd=directory)
+
+        status(directory, commit_hash, True)  # success
+
+
+def main():
+    git_sync_deps()
+
+
+if __name__ == '__main__':
+    exit(main())
