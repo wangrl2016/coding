5ddb03572f3b64ac0d1dfe255c38b264a61753e9
wangrl2016 <wangrl2016@gmail.com>
2021-03-10 03:59:24 +0800

    实现几种遍历方法

--- sdsa/binarytree/BinaryTreeTraversal.java
+++ sdsa/binarytree/BinaryTreeTraversal.java
@@ -48,6 +48,40 @@ public class BinaryTreeTraversal {
         return root;
     }
 
+    public static void preOrder(Node root) {
+        if (root == null)
+            return;
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null)
+            return;
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null)
+            return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    // 广度优先算法
+    public static void levelOrder(Node root) {
+
+    }
+
+    // 深度优先
+    public static void deepOrder(Node root) {
+
+    }
+
     public static void main(String[] args) {
         BinaryTreeTraversal tree = new BinaryTreeTraversal();
 
