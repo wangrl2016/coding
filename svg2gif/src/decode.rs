f23406d4bcc73791d531a28ced200c0cd773fac5
wangrl2016 <wangrl2016@gmail.com>
2021-03-18 20:02:49 +0800

    微小修正

--- svg2gif/src/decode.rs
+++ svg2gif/src/decode.rs
@@ -80,7 +80,8 @@ impl FrameContainer {
                             // 保存为ppm图片格式
                             println!("{}", timestamp);
                             if timestamp < 40 {
-                                save_ppm_file(&*format!("{}.ppm", timestamp), Pixel::RGB24, width, height, data);
+                                save_ppm_file(&*format!("{}.ppm", timestamp),
+                                              Pixel::RGB24, width, height, data);
                             }
                         }));
                     }
