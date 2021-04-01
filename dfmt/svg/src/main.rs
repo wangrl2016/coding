e44f699defd23e011af4290172de5a63ed452684
wangrl2016 <wangrl2016@gmail.com>
2021-04-02 00:25:24 +0800

    开始更新svg渲染程序

--- dfmt/svg/src/main.rs
+++ dfmt/svg/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = process() {}
+}
+
+fn process() -> Result<(), String> {
+    let args = match parse_args() {};
+}
+
+#[derive(Debug)]
+struct CliArgs {
+    help: bool,
+    version: bool,
+
+    width: Option<u32>,
+    height: Option<u32>,
+    zoom: Option<f32>,
+    dpi: u32,
+
+}
+
+fn collect_args() -> Result<CliArgs, pico_args::Error> {
+
+}
+
+fn parse_args() -> Result<Args, String> {
+    let mut args = collect_args().map_err(|e| e.to_string())?;
 }
